8c99820a9a8f2e3025b4be0d45deba5d66fd7685
Pufflet520 <pufflet520@gmail.com>
2025-04-06 13:51:20 +0800

    Update 3.py

--- 01000/3.py
+++ 01000/3.py
@@ -17,6 +17,7 @@ for i in range(1,int(input())+1):
       print(f'Command #{i}: "{b[1]}"')
     else:
       print(f'Command #{i}: "{b[1][0:c]+b[-2]+b[1][c+int(len(b[-4])):]}"')
+#https://judge.hkoi.org/submission/1162513/details?sharing=TbOjSRbDufYNhfCmVR244HJxNkA
 #評測結果 100.000
 #子任務	測試	結果	執行時間	記憶體	分數
 #1	1	正確	0.013 s	8.383 MB	100.000
